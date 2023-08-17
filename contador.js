73b2e7f6b9bb078a7755226dbbdba62a8d7d8887
narlison <nrtv10@gmail.com>
2023-08-16 22:10:20 -0300

    testedeversao

--- contador.js
+++ contador.js
@@ -1,32 +1,29 @@
-var dataInicio = new Date('2022-08-17')
+var dataInicio = new Date('2022-08-17T00:00:00-03:00');
 
-function atualizarContador(){
-    var dataAtual = new Date()
-    var diferencaTempo = dataAtual.getTime()- dataInicio.getTime()
-    var diferencaDias = Math.floor(diferencaTempo / (1000* 3600 * 24))
-    
-    var contadorElemento = document.getElementById('contador-dias')
+function atualizarContador() {
+    var dataAtual = new Date();
+    var diferencaTempo = dataAtual.getTime() - dataInicio.getTime();
+    var diferencaDias = Math.floor(diferencaTempo / (1000 * 3600 * 24));
+
+    var contadorElemento = document.getElementById('contador-dias');
     contadorElemento.textContent = diferencaDias;
+
+    var diferencaSegundos = Math.floor(diferencaTempo / 1000);
+    var segundos = diferencaSegundos % 60;
+    var minutos = Math.floor((diferencaSegundos / 60) % 60);
+    var horas = Math.floor(diferencaSegundos / 3600);
+
+    var contadorElementoTempo = document.getElementById('hora');
+    contadorElementoTempo.textContent = horas;
     
-    var diferencaSegundos = Math.floor(diferencaTempo / 1000)
-    var horas = Math.floor(diferencaSegundos /3600)
-    var minutos = Math.floor((diferencaSegundos % 3600) / 60)
-    var segundos = diferencaSegundos % 60
-    
-    /* RETORNA AS HORAS */
-    var contadorElementoTempo = document.getElementById('hora')
-contadorElementoTempo.textContent = horas;
-    /* RETORNA OS MINUTOS */
-    var contadorElementoTempo = document.getElementById('minuto')
+    contadorElementoTempo = document.getElementById('minuto'); // Corrigido aqui
     contadorElementoTempo.textContent = minutos;
-    /* RETORNA OS SEGUNDOS*/
-    var contadorElementoTempo = document.getElementById('segundo')
+    
+    contadorElementoTempo = document.getElementById('segundo'); // Corrigido aqui
     contadorElementoTempo.textContent = segundos;
 }
 
+atualizarContador();
+setInterval(atualizarContador, 1000);
 
-
-
-
-atualizarContador()
-setInterval(atualizarContador,1000)
+/* DESENVOLVIDO POR @NARLISONXL */
